c7a09b8230bc69d28787715ed800d36281bf1560
Sujit <sb2011ams@gmail.com>
2022-08-22 22:54:38 +0200

    added react-bootstrap and CARD

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,17 +4,18 @@ import PropTypes from "prop-types";
 export class MovieCard extends React.Component {
   render() {
       const { movie, onMovieClick } = this.props;
-      return (
-        <div
-          className="movie-card"
-              onClick={() => {
-                  onMovieClick(movie)
-              }
-          }
-        >
-          {movie.Title}
-        </div>
-      );
+          return (
+            <Card>
+              <Card.Img variant="top" src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Description}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="link">
+                  Open
+                </Button>
+              </Card.Body>
+            </Card>
+          );
   }
 }
 
